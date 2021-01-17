57cb7156920617cc919075cd43ff87f3583c8c74
asd <asd@Ramachandra>
2021-01-17 13:16:39 +0530

    tedt

---
+++ CompletePractice/src/main/java/SelfPractice/CompletePractice/Java_practice1.java
@@ -0,0 +1,18 @@
+package SelfPractice.CompletePractice;
+
+public class Java_practice1 {
+
+	public static void main(String[] args) {
+		
+		for(int j=1; j<=4; j++) {
+			for(int i=1; i<=4;i++) {
+				System.out.println(i + " ");
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
